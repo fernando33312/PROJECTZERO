c529633907fe16ec75255dbf533d019af7fa5820
Luis Fernando <luis.fernando.accelerator@hotmail.com>
2020-05-13 20:51:35 -0500

    agregando NavBar

---
+++ miprimerapp/src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./NavBar.css";
+
+//Arrow Function
+const NavBar = () => {
+
+    return (
+        <div className="NavBar">
+        <div>
+            Inicio
+        </div>
+    
+        <div>
+            Tienda
+        </div>
+        <div>
+            Manga
+        </div>
+        <div>
+            Reseñas
+        </div>
+        </div>
+    )
+}
+export default NavBar;
